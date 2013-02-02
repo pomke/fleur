1d4bdf5496a0755c7c89e68299e3873436c59c18
Pomke <pomke@pangur.com.au>
2013-02-02 19:18:36 +1100

    added start of setup dialog

--- options.js
+++ options.js
@@ -1,17 +1,32 @@
 /* Themes should be listed here */
-var themes = {
-
-    fleur : {
+var themesList = [
+    {
         title : "Fleur Default Theme",
         version : 0.1,
         author : "Pomke Nohkan <pomke@pangur.com.au>",
         template : 'themes/fleur/index.html',
     }
 
-};
+];
 
-var backends = {
-    staticFile :  {
-
+var backendList = [
+    {
+        title : "Static Files",
+        code : "/backends/static.js"
+    },
+    {
+        title : "Gists",
+        code : "/backends/gist.js"
     }
+];
+
+var themes = {};
+for(var i=0; i < themesList.length; i++) {
+    var t = themesList[i];
+    themes[t.title] = t;
+}
+var backends = {};
+for(var i=0; i < backendList.length; i++) {
+    var b = backendList[i];
+    backends[b.title] = b;
 }
